1f268545f35ef5ba0c611fedb3bc52e31a41307e
Denis Rozhkov <rojkoff@gmail.com>
2022-01-15 00:21:01 +0300

    task solution revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let s;
+    let newN;
+    if (n > 0) {
+        s = String(n).split("").reverse().join("");
+        newN = Number(s);
+    } else {
+        s = String(n).split("").slice(1).reverse().join("");
+        newN = Number(s);
+    }
+
+    return newN;
+};
